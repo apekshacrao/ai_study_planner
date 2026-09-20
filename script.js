51d8908cce8c597a691573cbf5de37dc55fbf599
apekshacrao <apekshacrao@gmail.com>
2026-09-20 21:32:51 +0530

    creted file structure

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const button = document.getElementById('start-btn');
+
+  if (button) {
+    button.addEventListener('click', () => {
+      alert('Study plan feature coming soon!');
+    });
+  }
+});
